f12567fcafbe7af51289c82dad70b07c95ce513c
DW Rodrigues <denys.w.r@hotmail.com>
2021-04-12 19:00:50 -0300

    Correção - Exercício 3

--- Trabalho1/Trabalho1/Tela3.aspx.cs
+++ Trabalho1/Trabalho1/Tela3.aspx.cs
@@ -16,37 +16,59 @@ namespace Trabalho1
 
         protected void btnVerificar_Click(object sender, EventArgs e)
         {
-            int nasc, datatual;
-            //int dia, mes, ano;
+            int /*nasc,*/ dataatual, niver;
+            int /*dia, mes,*/ ano;
 
             //dia = Convert.ToInt32(txtDia.Text);
             //mes = Convert.ToInt32(txtMes.Text);
-            //ano = Convert.ToInt32(txtAno.Text);
+            ano = Convert.ToInt32(txtAno.Text);
 
             //nasc = {dia} + {mes} + {ano};
 
-            nasc = Convert.ToInt32(txtNascimento.Text);
+            //nasc = Convert.ToInt32(txtNascimento.Text);
 
-            datatual = 12042021;
+            //dataatual = 12042021;
+            dataatual = 2021;
 
-            if (ddlSexo.Text == "F")
-            {
-                lblMSG.Text = "Dispensada";
-            }
-            else if (nasc < datatual)
-            {
-                if (ddlSexo.Text == "M")
-                {
-                    lblMSG.Text = "Idade Insuficiente";
-                }
-            }
-            else if (nasc >= datatual)
-            {
-                if (ddlSexo.Text == "M")
+            niver = dataatual - ano;
+
+            //niver = dataatual - nasc
+
+            if (ddlSexo.Text == "M")
+                if (niver >= 18)
                 {
                     lblReservista.Text = "Cart. Reservista";
+                    lblMSG.Text = string.Empty;
                 }
+            else
+                {
+                    lblMSG.Text = "Dispensado";
+                    lblReservista.Text = string.Empty;
+                }
+            if (ddlSexo.Text == "F")
+                {
+                    lblMSG.Text = "Dispensada";
+                    lblReservista.Text = string.Empty;
             }
+
+            //if (ddlSexo.Text == "F")
+            //{
+            //    lblMSG.Text = "Dispensada";
+            //}
+            //else if (nasc < datatual)
+            //{
+            //    if (ddlSexo.Text == "M")
+            //    {
+            //        lblMSG.Text = "Idade Insuficiente";
+            //    }
+            //}
+            //else if (nasc >= datatual)
+            //{
+            //    if (ddlSexo.Text == "M")
+            //    {
+            //        lblReservista.Text = "Cart. Reservista";
+            //    }
+            //}
         }
     }
 }
--- Trabalho1/Trabalho1/Tela3.aspx.designer.cs
+++ Trabalho1/Trabalho1/Tela3.aspx.designer.cs
@@ -24,22 +24,58 @@ namespace Trabalho1
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// Controle lblNascimento.
+        /// Controle lblDia.
         /// </summary>
         /// <remarks>
         /// Campo gerado automaticamente.
         /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblNascimento;
+        protected global::System.Web.UI.WebControls.Label lblDia;
 
         /// <summary>
-        /// Controle txtNascimento.
+        /// Controle txtDia.
         /// </summary>
         /// <remarks>
         /// Campo gerado automaticamente.
         /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtNascimento;
+        protected global::System.Web.UI.WebControls.TextBox txtDia;
+
+        /// <summary>
+        /// Controle lblMes.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMes;
+
+        /// <summary>
+        /// Controle txtMes.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtMes;
+
+        /// <summary>
+        /// Controle lblAno.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAno;
+
+        /// <summary>
+        /// Controle txtAno.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAno;
 
         /// <summary>
         /// Controle lblSexo.
